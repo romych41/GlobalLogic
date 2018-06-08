e2bbc8c2ecbe2c4f588678e24620f54179a79e57
romych41 <romych41@gmail.com>
2018-06-08 17:21:17 +0300

    №7
    Задание №7

---
+++ №7/№7/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void InputData(int*, int*);
+void LenghtCalc(int*, int*, int*, int*);
+void OutputData(int*, int*);
+
+int main()
+{
+	int d, n, lenght = 0, countOfOperation = 0;
+	InputData(&d, &n);
+	LenghtCalc(&d, &n, &lenght, &countOfOperation);
+	OutputData(&lenght, &countOfOperation);
+	getch();
+	return 0;
+}
+
+void InputData(int *d, int *n)
+{
+	printf("Input the lenght between the sensors: ");
+	scanf_s("%d", d);
+	printf("Input the number of sensors: ");
+	scanf_s("%d", n);
+}
+
+void LenghtCalc(int *d, int *n, int *lenght, int *countOfOperation)//The complexity of the algorithm is O(2N)
+{
+	for (int i = *n; i > 0; i--)
+	{
+		*lenght += ((*d)*i);
+		*countOfOperation+=2;
+	}
+}
+
+void OutputData(int *lenght, int *countOfOperation)
+{
+	printf("Wire lenght: %d", *lenght);
+	printf("\nCount of operation: %d", *countOfOperation);
+}
